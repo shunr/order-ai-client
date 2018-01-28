75b720da14aae32912315b5462549685a85049a1
Shun Yu Rao <syrao@uwaterloo.ca>
2018-01-28 00:35:39 -0500

    restart if server dies

--- app.js
+++ app.js
@@ -4,26 +4,32 @@ const printer = require('./printer');
 const server = new WebSocket('ws://162.243.171.232:8080/orders');
 
 let app = module.exports = {};
+let orders = [];
 
 app.run = async () => {
   try {
     let paper = await printer.init();
-    server.on('message', (data) => {
-        let order = JSON.parse(data);
-        console.log(order);
-        printer.print(paper, 'Restaurant Name\n', 2);
-        printer.print(paper, order.contact);
-        for (let i = 0; i < order.items.length; i++) {
-          let item = order.items[i];
-          printer.print(paper, item.amount + ' ' + item.name + ' ' + '$0.00');
-        }
-        printer.cut(paper);
-    });
   } catch (err) {
     console.error(err);
+    process.exit();
   }
+  server.on('message', (data) => {
+    let order = JSON.parse(data);
+    console.log(order);
+    orders.push(order);
+    printer.print(paper, 'Restaurant Name\n', 2);
+    printer.print(paper, order.contact);
+    for (let i = 0; i < order.items.length; i++) {
+      let item = order.items[i];
+      printer.print(paper, item.amount + ' ' + item.name + ' ' + '$0.00');
+    }
+    printer.print(paper, 'Total: ' + '$' + order.total);
+    printer.cut(paper);
+  });
+
+  ws.on('close', () => {
+    process.exit();
+  });
 };
 
 app.run();
-
-
