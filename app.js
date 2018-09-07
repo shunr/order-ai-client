d6fcc4793fc0b85ba6879d44cd235304d6a71375
Shun Rao <syrao@uwaterloo.ca>
2018-09-07 00:05:34 +0000

    For pizza demo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 const printer = require('./printer');
 const express = require('express')
 
-const server = new WebSocket('ws://162.243.171.232:8080/orders');
+const server = new WebSocket('ws://162.243.171.232:9001/orders');
 const app = express();
 
 let orders = [];
@@ -16,16 +16,21 @@ app.run = async () => {
     console.error(err);
     process.exit();
   }
+  console.log('started');
 
   server.on('message', (data) => {
     let order = JSON.parse(data);
     console.log(order);
     orders.push(order);
-    printer.print(paper, 'Restaurant Name\n', 2);
+    printer.print(paper, 'Pizza Place\n', 2);
     printer.print(paper, order.contact);
     for (let i = 0; i < order.items.length; i++) {
       let item = order.items[i];
-      printer.print(paper, item.amount + ' ' + item.name + ' ' + '$0.00');
+      let toppings = '';
+      for (let j = 0; j < item.toppings.length; j++) {
+        toppings += item.toppings[j] + ' ';
+      }
+      printer.print(paper, item.amount + ' ' + toppings + '$0.00');
     }
     printer.print(paper, 'Total: ' + '$' + order.total);
     printer.cut(paper);
